98da4690b1aba3c2b32fb07676d1cc28eb83e7e8
JemzyD <jemima.ly.lim@gmail.com>
2018-08-28 02:03:53 +0800

    original

--- server/server.js
+++ server/server.js
@@ -3,23 +3,11 @@ import app from './express'
 import mongoose from 'mongoose'
 
 // Connection URL
-// mongoose.Promise = global.Promise
-// mongoose.connect(config.mongoUri)
-// mongoose.connection.on('error', () => {
-//   throw new Error(`unable to connect to database: ${mongoUri}`)
-// })
-
-// mongoose.connect(dbURI)
-mongoose.Promise = global.Promise;
-mongoose.set('debug', true);
-mongoose
-.connect(CONNECTION_URI, {
-  useMongoClient: true
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri)
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
 })
-.then(() => {
-  console.log('connected to mongodb');
-})
-.catch(err => console.log(err));
 
 app.listen(config.port, (err) => {
   if (err) {
